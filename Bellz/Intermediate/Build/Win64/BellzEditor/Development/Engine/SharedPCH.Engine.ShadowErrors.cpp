3dd6c8d502a05b2e549403160854e04510c34aea
나란히 <lazpberry1012@gmail.com>
2022-04-24 23:03:42 +0900

    Unreal Project "Bellz" Initialize
    Unreal Engine 4 with C++
    Title: Bellz

---
+++ Bellz/Intermediate/Build/Win64/BellzEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30141
+#include "C:/Users/Home/Desktop/Projects/Basic-Game-Development/Bellz/Intermediate/Build/Win64/BellzEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Bellz/Intermediate/Build/Win64/UE4Editor/Inc/Bellz/BellzGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BELLZ_BellzGameModeBase_generated_h
+#error "BellzGameModeBase.generated.h already included, missing '#pragma once' in BellzGameModeBase.h"
+#endif
+#define BELLZ_BellzGameModeBase_generated_h
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_SPARSE_DATA
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_RPC_WRAPPERS
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABellzGameModeBase(); \
+	friend struct Z_Construct_UClass_ABellzGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABellzGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Bellz"), NO_API) \
+	DECLARE_SERIALIZER(ABellzGameModeBase)
+
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABellzGameModeBase(); \
+	friend struct Z_Construct_UClass_ABellzGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABellzGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Bellz"), NO_API) \
+	DECLARE_SERIALIZER(ABellzGameModeBase)
+
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABellzGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABellzGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABellzGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABellzGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABellzGameModeBase(ABellzGameModeBase&&); \
+	NO_API ABellzGameModeBase(const ABellzGameModeBase&); \
+public:
+
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABellzGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABellzGameModeBase(ABellzGameModeBase&&); \
+	NO_API ABellzGameModeBase(const ABellzGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABellzGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABellzGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABellzGameModeBase)
+
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Bellz_Source_Bellz_BellzGameModeBase_h_12_PROLOG
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_SPARSE_DATA \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_RPC_WRAPPERS \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_INCLASS \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Bellz_Source_Bellz_BellzGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_SPARSE_DATA \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Bellz_Source_Bellz_BellzGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BELLZ_API UClass* StaticClass<class ABellzGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Bellz_Source_Bellz_BellzGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
